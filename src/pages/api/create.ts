966b2f1441e9f675137d236899263304ffe39ab4
SokWith <129967025+SokWith@users.noreply.github.com>
2024-02-05 23:09:05 +0800

    Update create.ts

--- src/pages/api/create.ts
+++ src/pages/api/create.ts
@@ -12,6 +12,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       ...req.cookies,
       BING_IP: randomIP()
     })
+    console.log(${header})
     do {
       const endpoints = [req.headers['x-endpoint'], ...(process.env.ENDPOINT || '').split(','), 'www.bing.com'].filter(Boolean)
       const endpoint = endpoints[count % endpoints.length]
